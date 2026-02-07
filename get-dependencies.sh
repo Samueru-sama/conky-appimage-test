0e4355ac133c81aeaab2faf0d0b47c54d247834b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-07 00:35:19 -0400

    add `audacious`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm conky
+pacman -Syu --noconfirm audacious
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
