0b3a9997f1f96a5dd02eb84e24da0ada978881be
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-07 00:31:53 -0400

    try to build conky with audacious support
    https://github.com/ivan-hc/AM/issues/2055#issuecomment-3863544155

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm conky
+#pacman -Syu --noconfirm conky
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+export PRE_BUILD_CMDS="
+	sed -i -e 's|-D BUILD_DOCS=ON|-D BUILD_DOCS=ON -DBUILD_AUDACIOUS=ON|' ./PKGBUILD
+"
+make-aur-package --archlinux-pkg conky
 
 # If the application needs to be manually built that has to be done down here
 
